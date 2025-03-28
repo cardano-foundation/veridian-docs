84408aa1322670bafe14e6f23e951e8150b59336
Jorge Navarro Bendicho <jorgenavben@protonmail.com>
2025-03-28 10:45:55 +0100

    feat: add content separators for different sections

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,7 +1,44 @@
 export default {
+  _: {
+    type: "separator",
+    title: "Veridian",
+  },
   index: "",
-  walletGuide: "Veridian Wallet User Guide",
-  infra: "Infrastructure",
-  devs: "Developer Guide",
-  support: "Support"
+  __: {
+    type: "separator",
+    title: "Veridian Wallet User Guide",
+  },
+  walletGuide: "Introduction",
+  onboarding: {
+    theme: {
+      collapsed: true,
+    },
+  },
+  fundamentals: {
+    theme: {
+      collapsed: true,
+    },
+  },
+  useFlows: {
+    title: "Use Flows",
+    theme: {
+      collapsed: true,
+    },
+  },
+  ___: {
+    type: "separator",
+    title: "Infrastructure",
+  },
+  sandbox: "Hosted Sandbox",
+  selfdeployment: "Self Deployment",
+  ____: {
+    type: "separator",
+    title: "Developer Guide",
+  },
+  devs: "Introduction",
+  _____: {
+    type: "separator",
+    title: "More",
+  },
+  support: "Support",
 };
--- content/walletGuide/_meta.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  onboarding: "",
-  fundamentals: "",
-  useFlows: "Use Flows",
-};
